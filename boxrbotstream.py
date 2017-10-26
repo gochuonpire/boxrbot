c0ad28a84d5bd5dc68dd160db8af22702a69714a
Andrew Bulkeley <gochuonpire@gmail.com>
2017-10-26 17:06:54 -0400

    idk

--- boxrbotstream.py
+++ boxrbotstream.py
@@ -12,43 +12,46 @@ def r_gen(size=7, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 async def checkStream():
-    server = bot.get_server('106386168593010688')
-    client = TwitchClient('twitch_api_client_id')
-    stream = client.streams.get_stream_by_user('twitch_channel_id')
-    vc = server.get_channel('141316972750045185')
     try:
-        if(stream.id is not None):
+        server = bot.get_server('106386168593010688')
+        client = TwitchClient('twitch_api_client_id')
+        stream = client.streams.get_stream_by_user('twitch_channel_id')
+        vc = server.get_channel('141316972750045185')
+        try:
+            if(stream.id is not None):
+                flag = 1
+                async for message in bot.logs_from(vc, 500):
+                    if(message.author.id == '362978129301471234'):
+                        if(len(message.embeds)>0):
+                            flag = 0
+                        else:
+                            await bot.delete_message(message)
+                if(flag == 1):
+                    eMsg = discord.Embed(title=stream.channel.status, url=stream.channel.url, description="Playing: " + stream.channel.game)
+                    rando = r_gen()
+                    imgUrl = stream.preview["large"] + "?test=" + rando
+                    eMsg.set_image(url=imgUrl)
+                    eMsg.set_author(name="launders is live!")
+                    await bot.send_message(vc, '', embed=eMsg)
+                    await updateUsers(eMsg)
+
+        except AttributeError:
             flag = 1
             async for message in bot.logs_from(vc, 500):
                 if(message.author.id == '362978129301471234'):
                     if(len(message.embeds)>0):
-                        flag = 0
-                    else:
                         await bot.delete_message(message)
-            if(flag == 1):
-                eMsg = discord.Embed(title=stream.channel.status, url=stream.channel.url, description="Playing: " + stream.channel.game)
-                rando = r_gen()
-                imgUrl = stream.preview["large"] + "?test=" + rando
-                eMsg.set_image(url=imgUrl)
-                eMsg.set_author(name="launders is live!")
-                await bot.send_message(vc, '', embed=eMsg)
-                await updateUsers(eMsg)
-
-    except AttributeError:
-        flag = 1
-        async for message in bot.logs_from(vc, 500):
-            if(message.author.id == '362978129301471234'):
-                if(len(message.embeds)>0):
-                    await bot.delete_message(message)
-                else:
-                    flag = 0
-                if(flag == 1):
-                    flag = 0
+                    else:
+                        flag = 0
+                    if(flag == 1):
+                        flag = 0
+                        msg = "launders is offline! You can message " + bot.user.mention + " with **.subscribe** for a message every time he goes live!:pager:"
+                        await bot.send_message(vc, msg)
+                if flag == 1:
                     msg = "launders is offline! You can message " + bot.user.mention + " with **.subscribe** for a message every time he goes live!:pager:"
                     await bot.send_message(vc, msg)
-            if flag == 1:
-                msg = "launders is offline! You can message " + bot.user.mention + " with **.subscribe** for a message every time he goes live!:pager:"
-                await bot.send_message(vc, msg)
+    except:
+        print("Twitch broke boi idk or something")
 
 async def updateUsers(msg):
     server = bot.get_server('106386168593010688')
